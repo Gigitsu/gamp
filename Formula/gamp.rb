9f74829c34cdaccfe0103adf660d2be2dff11623
Gigitsu <gigitsu.23@gmail.com>
2015-02-06 21:49:41 +0100

    Added sha1 checksum

--- Formula/gamp.rb
+++ Formula/gamp.rb
@@ -4,6 +4,7 @@ class Gamp < Formula
   homepage 'https://github.com/Gigitsu/homebrew-gamp'
   url 'https://raw.githubusercontent.com/Gigitsu/homebrew-gamp/master/gamp'
   version '1.0-beta'
+  sha1 'b2c07cae10300d84dabaa30f2f605f0d521b3d1b'
 
   def install
     libexec.install "gamp"
