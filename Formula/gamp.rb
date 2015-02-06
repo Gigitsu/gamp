dfb9a95f28e7dfdf850e2a623739048d929bff6b
Gigitsu <gigitsu.23@gmail.com>
2015-02-06 16:37:43 +0100

    First code attempt

--- Formula/gamp.rb
+++ Formula/gamp.rb
@@ -1,5 +1,15 @@
 require "formula"
 
 class Gamp < Formula
+  homepage 'https://github.com/Gigitsu/homebrew-gamp'
+  url 'https://github.com/Gigitsu/homebrew-gamp/blob/master/gamp'
+  version '1.0-alpha'
+
+  def install
+    opoo #{HOMEBREW_PREFIX}+"Cellar"
+    sh = libexec + "gamp"
+    chmod 0755, sh
+    bin.install_symlink sh
+  end
 
 end
